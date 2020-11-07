2bbfa42c6c49cc378a1bd8e5cac4e737ae7dc78e
Belayet-aaroza <belayet.aaroza@gmail.com>
2020-11-07 17:50:15 +0600

    add modal

--- src/views/component/card.js
+++ src/views/component/card.js
@@ -4,16 +4,7 @@ export default function card(props) {
     let { cards, openModal, setShowModal } = props;
     return (
         <div>
-            <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }}>
-            <div className="card-header d-flex flex-column">
-                        <i className="fab fa-facebook-messenger" style={{ color: "#18dede", fontSize: 24 }}></i>
-                        <span style={{ fontSize: 12 }}>Feedback</span>
-                        <p>Send Message</p>
-                    </div>
-                <div className="card-body">Add Text</div>
-                <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>Next Step</div>
-            </div>
-            {/* {cards.map((card, index) => (
+            {cards.map((card, index) => (
                 <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }} key={card.id} onClick={() => openModal(card.id)}>
                     <div className="card-header d-flex flex-column">
                         <i className="fab fa-facebook-messenger" style={{ color: "#18dede", fontSize: 24 }}></i>
@@ -35,7 +26,7 @@ export default function card(props) {
                     </div>
                     <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>Next Step</div>
                 </div>
-            ))} */}
+            ))}
 
 
         </div>
--- src/views/component/flow.js
+++ src/views/component/flow.js
@@ -1,56 +1,71 @@
 import React, { useState, useEffect } from 'react'
 import Card from './card';
+import Modal from './modal';
 import ReactFlow, {
-    isEdge,
     removeElements,
     addEdge,
-    Handle,
     MiniMap,
     Controls,
     Background,
 } from 'react-flow-renderer';
 
 let count = 2;
-const initialElements = [
-    {
-        id: '1',
-        type: 'default',
-        data: {
-            label: (
-                <>
-                    <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }}>
-                        <div className="card-header">
-                            <i className="fas fa-play-circle" style={{ color: "#16bf16", fontSize: 24 }}></i>
-                            <p>Starting Step</p>
-                        </div>
-                        <div className="card-body">Add trigger</div>
-                        <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>The First Step</div>
-                    </div>
-                </>
-            ),
-        },
-        position: { x: 10, y: 100 },
-        style: { width: 265 },
-    },
-    {
-        id: '2',
-        data: {
-            label: (
-                <>
-                    <Card />
-                </>
-            ),
-        },
-        position: { x: 350, y: 100 },
-        style: { width: 265 },
-    },
-];
+
 const graphStyles = { width: '100%', height: '700px' };
 
 const onLoad = (reactFlowInstance) => {
     reactFlowInstance.fitView();
 };
 function Flow() {
+    const [showModal, setShowModal] = useState(false);
+    const [cards, setCards] = useState([{ id: 1, text: 'Add Text', picture: [] }]);
+    const [card, setCard] = useState({});
+    
+    const openModal = (id) => {
+        let text = '';
+        let picture = [];
+        let index = cards.findIndex(el => el.id === id);
+        if (index !== -1) {
+            text = cards[index].text;
+            picture = cards[index].picture;
+        }
+        setCard({ id, text, picture });
+        setShowModal(!showModal)
+    }
+    const initialElements = [
+        {
+            id: '1',
+            type: 'default',
+            data: {
+                label: (
+                    <>
+                        <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }} onDoubleClick={() => setShowModal(true)}>
+                            <div className="card-header">
+                                <i className="fas fa-play-circle" style={{ color: "#16bf16", fontSize: 24 }}></i>
+                                <p>Starting Step</p>
+                            </div>
+                            <div className="card-body">Add trigger</div>
+                            <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>The First Step</div>
+                        </div>
+                    </>
+                ),
+            },
+            position: { x: 10, y: 100 },
+            style: { width: 265 },
+        },
+        {
+            id: '2',
+            data: {
+                label: (
+                    <>
+                        <Card cards={cards} openModal={openModal} setShowModal={setShowModal} />
+                    </>
+                ),
+            },
+            position: { x: 350, y: 100 },
+            style: { width: 265 },
+        },
+    ];    
     const [elements, setElements] = useState(initialElements);
 
     const onElementsRemove = (elementsToRemove) =>
@@ -58,11 +73,11 @@ function Flow() {
     const onConnect = (params) => setElements((els) => addEdge(params, els));
     const addcard = () => {
         setElements([...elements, {
-            id: count+1,
+            id: count + 1,
             data: {
                 label: (
                     <>
-                        <Card />
+                        <Card cards={cards} openModal={openModal} setShowModal={setShowModal} />
                     </>
                 ),
             },
@@ -71,11 +86,37 @@ function Flow() {
         }])
         count++;
     }
+    const handleChange = (e) => {
+        let newCard = { id: card.id, picture: card.picture };
+
+        if (e.target.type === 'text') newCard.text = e.target.value;
+        else newCard.text = card.text;
+
+        setCard(newCard);
+        let index = cards.findIndex(el => el.id === card.id);
+
+        if (index !== -1) {
+            if (e.target.type === 'text') {
+                cards[index].text = e.target.value;
+            } else {
+                let reader = new FileReader();
+                reader.onloadend = () => {
+                    cards[index].picture = [...card.picture, {
+                        file: e.target.files[0],
+                        imagePreviewUrl: reader.result
+                    }]
+                }
+                reader.readAsDataURL(e.target.files[0])
+            }
+        }
+
+    }
 
 
     return (
         <>
             <i className="fas fa-plus-circle" style={{ color: "#008000", fontSize: 50, margin: 20, cursor: "pointer" }} onClick={addcard}></i>
+            <Modal card={card} handleChange={handleChange} setShowModal={setShowModal} setCard={setCard} showModal={showModal}></Modal>
             <ReactFlow
                 elements={elements}
                 style={graphStyles}
