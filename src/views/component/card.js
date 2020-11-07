5e6c2734bde4bae20869ef20cb280b3dafbbc082
Belayet-aaroza <belayet.aaroza@gmail.com>
2020-11-07 16:20:37 +0600

    fix

--- src/views/component/card.js
+++ src/views/component/card.js
@@ -5,13 +5,39 @@ export default function card(props) {
     return (
         <div>
             <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }}>
-                <div className="card-header d-flex flex-column">
-                    <i className="fab fa-facebook-messenger" style={{ color: "#18dede", fontSize: 24 }}></i>
-                    <span style={{ fontSize: 12 }}>Feedback</span>
-                    <p>Send Message</p>
-                </div>
+            <div className="card-header d-flex flex-column">
+                        <i className="fab fa-facebook-messenger" style={{ color: "#18dede", fontSize: 24 }}></i>
+                        <span style={{ fontSize: 12 }}>Feedback</span>
+                        <p>Send Message</p>
+                    </div>
+                <div className="card-body">Add Text</div>
                 <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>Next Step</div>
             </div>
+            {/* {cards.map((card, index) => (
+                <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }} key={card.id} onClick={() => openModal(card.id)}>
+                    <div className="card-header d-flex flex-column">
+                        <i className="fab fa-facebook-messenger" style={{ color: "#18dede", fontSize: 24 }}></i>
+                        <span style={{ fontSize: 12 }}>Feedback</span>
+                        <p>Send Message</p>
+                    </div>
+                    <div className="card-body">
+                        {card.text}
+                        {card.picture ? card.picture.map((pic, index) => {
+                            if (pic.imagePreviewUrl) {
+                                return (
+                                    <div key={index}>
+                                        <hr />
+                                        <img src={pic.imagePreviewUrl} alt=''style={{ width: 150, height: 150 }} />
+                                    </div>
+                                );
+                            }
+                        }) : null}
+                    </div>
+                    <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>Next Step</div>
+                </div>
+            ))} */}
+
+
         </div>
     )
 }
