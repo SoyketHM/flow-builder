d2faf118b65b82c5e31af43b3bb6fc0a6aff9e04
Belayet-aaroza <belayet.aaroza@gmail.com>
2020-11-07 16:19:38 +0600

    add card added

--- src/views/component/flow.js
+++ src/views/component/flow.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Card from './card';
 import ReactFlow, {
     isEdge,
     removeElements,
@@ -9,6 +10,7 @@ import ReactFlow, {
     Background,
 } from 'react-flow-renderer';
 
+let count = 2;
 const initialElements = [
     {
         id: '1',
@@ -16,12 +18,6 @@ const initialElements = [
         data: {
             label: (
                 <>
-                    <Handle
-                        type="target"
-                        position="top"
-                        style={{ background: '#555' }}
-                        onConnect={(params) => console.log('handle onConnect', params)}
-                    />
                     <div className="card" style={{ width: 200, margin: 20, cursor: "pointer" }}>
                         <div className="card-header">
                             <i className="fas fa-play-circle" style={{ color: "#16bf16", fontSize: 24 }}></i>
@@ -30,16 +26,10 @@ const initialElements = [
                         <div className="card-body">Add trigger</div>
                         <div className="card-footer" style={{ fontSize: 12, textAlign: "right" }}>The First Step</div>
                     </div>
-                    <Handle
-                        type="source"
-                        position="right"
-                        id="a"
-                        style={{ top: 10, background: '#555' }}
-                    />
                 </>
             ),
         },
-        position: { x: 250, y: 0 },
+        position: { x: 10, y: 100 },
         style: { width: 265 },
     },
     {
@@ -47,77 +37,13 @@ const initialElements = [
         data: {
             label: (
                 <>
-                    <Handle
-                        type="target"
-                        id="t2"
-                        style={{ background: '#555' }}
-                    />
-                    This is <strong>node 2</strong>
-                    <Handle
-                        type="source"
-                        id="s2"
-                        style={{ background: '#555' }}
-                    />
+                    <Card />
                 </>
             ),
         },
-        position: { x: 50, y: 100 },
+        position: { x: 350, y: 100 },
+        style: { width: 265 },
     },
-    {
-        id: '3',
-        data: {
-            label: (
-                <>
-                    This one has a <strong>custom style</strong>
-                </>
-            ),
-        },
-        position: { x: 400, y: 300 },
-        style: {
-            background: '#D6D5E6',
-            color: '#333',
-            border: '1px solid #222138',
-            width: 180,
-        },
-    },
-    {
-        id: '4',
-        position: { x: 250, y: 400 },
-        data: {
-            label: 'Another default node 1',
-        },
-        sourcePosition: 'right',
-    },
-    {
-        id: '5',
-        position: { x: 300, y: 500 },
-        data: {
-            label: 'Another default node 2',
-        },
-        targetPosition: 'top',
-        sourcePosition: 'right',
-    },
-    {
-        id: '6',
-        position: { x: 300, y: 600 },
-        data: {
-            label: (
-                <>
-                    <Handle
-                        type="target"
-                        id="6s"
-                        style={{ background: '#555' }}
-                    />
-                    This is a <strong>new node</strong>
-                    <Handle
-                        type="target"
-                        id="6t"
-                        style={{background: '#555' }}
-                    />
-                </>
-            ),
-        },
-    }
 ];
 const graphStyles = { width: '100%', height: '700px' };
 
@@ -126,39 +52,57 @@ const onLoad = (reactFlowInstance) => {
 };
 function Flow() {
     const [elements, setElements] = useState(initialElements);
-    
+
     const onElementsRemove = (elementsToRemove) =>
-      setElements((els) => removeElements(elementsToRemove, els));
+        setElements((els) => removeElements(elementsToRemove, els));
     const onConnect = (params) => setElements((els) => addEdge(params, els));
-    
+    const addcard = () => {
+        setElements([...elements, {
+            id: count+1,
+            data: {
+                label: (
+                    <>
+                        <Card />
+                    </>
+                ),
+            },
+            position: { x: Math.floor(Math.random() * Math.floor(700)), y: Math.floor(Math.random() * Math.floor(600)) },
+            style: { width: 265 },
+        }])
+        count++;
+    }
+
 
     return (
-        <ReactFlow
-            elements={elements}
-            style={graphStyles}
-            onElementsRemove={onElementsRemove}
-            onConnect={onConnect}
-            onLoad={onLoad}
-            snapToGrid={true}
-            snapGrid={[15, 15]}
-        >
-            <MiniMap
-                nodeStrokeColor={(n) => {
-                    if (n.style?.background) return n.style.background;
-                    if (n.type === 'input') return '#0041d0';
-                    if (n.type === 'output') return '#ff0072';
-                    if (n.type === 'default') return '#1a192b';
-                    return '#eee';
-                }}
-                nodeColor={(n) => {
-                    if (n.style?.background) return n.style.background;
-                    return '#fff';
-                }}
-                nodeBorderRadius={2}
-            />
-            <Controls />
-            <Background color="#aaa" gap={16} />
-        </ReactFlow>
+        <>
+            <i className="fas fa-plus-circle" style={{ color: "#008000", fontSize: 50, margin: 20, cursor: "pointer" }} onClick={addcard}></i>
+            <ReactFlow
+                elements={elements}
+                style={graphStyles}
+                onElementsRemove={onElementsRemove}
+                onConnect={onConnect}
+                onLoad={onLoad}
+                snapToGrid={true}
+                snapGrid={[15, 15]}
+            >
+                <MiniMap
+                    nodeStrokeColor={(n) => {
+                        if (n.style?.background) return n.style.background;
+                        if (n.type === 'input') return '#0041d0';
+                        if (n.type === 'output') return '#ff0072';
+                        if (n.type === 'default') return '#1a192b';
+                        return '#eee';
+                    }}
+                    nodeColor={(n) => {
+                        if (n.style?.background) return n.style.background;
+                        return '#fff';
+                    }}
+                    nodeBorderRadius={2}
+                />
+                <Controls />
+                <Background color="#aaa" gap={16} />
+            </ReactFlow>
+        </>
     );
 }
 
